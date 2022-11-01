ede2e6633c9dd8058d88593ebc04b700de2b30be
vapnguyen24 <vinh.work@gmail.com>
2022-11-01 19:51:51 +0700

    add new

---
+++ config.js
@@ -0,0 +1,11 @@
+const nameGirl = 'Tuyền';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Quà nè! Mở ik';
+const titleCard = 'Tặng người ấy';
+// const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const contentCard = 'Cuối cùng cũng hết một ngày đi chơi rồi ha, chắc giờ Tuyền cũng mệt mỏi rùi, hy vọng chiếc web đơn giản này có thể giúp honey của anh trở nên thư giãn và thoải mái hơn'
+
+
+const giftImage = 'hot-girl.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
